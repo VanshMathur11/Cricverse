cb8c6f7af087e9c8718e8acd371ea088c1e834df
Vansh Mathur <vanshmathur00@gmail.com>
2026-08-28 16:47:31 +0530

    chore: initialize CricVerse project structure

---
+++ cricverse-backend/src/main/java/com/cricverse/cricverse_backend/CricverseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cricverse.cricverse_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricverseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CricverseBackendApplication.class, args);
+	}
+
+}
---
+++ cricverse-backend/src/test/java/com/cricverse/cricverse_backend/CricverseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cricverse.cricverse_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CricverseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
